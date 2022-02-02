8f031bc15f1547ec479d5183e277100fa26a9994
SUANUSHA <98738330+SUANUSHA@users.noreply.github.com>
2022-02-02 21:47:13 +0530

    anu

---
+++ macro14.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<string.h>
+#define MACRO(num) ++num 
+int main()
+{
+	char *ptr = "preprocessor";
+	int num =strlen(ptr);
+	printf("%s  ", MACRO(ptr));
+	printf("%d", MACRO(num));
+	return 0;
+}
+output: reprocessor  13
